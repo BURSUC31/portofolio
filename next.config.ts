d0fa303a4be0e3fcdf7ad1874f264d94f67ff544
Dimitrie Tomulesei <tomuleseidimitrie@gmail.com>
2025-07-03 19:45:32 +0300

    Deploy: Updated portfolio - 2025-07-03 19:45:32

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/dimitrie-portfolio-website-eu',
   assetPrefix: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu',
   images: {
     unoptimized: true,

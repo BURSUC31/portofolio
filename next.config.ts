02e9e38f7c0c23c6077a6a11c2210081ee272dd8
Dimitrie Tomulesei <tomuleseidimitrie@gmail.com>
2025-07-04 11:06:05 +0300

    Deploy: Updated portfolio - 2025-07-04 11:06:05

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
         port: '',
-        pathname: isProd ? '/dimitrie-portfolio-website-eu/**' : '/public',
+        pathname: isProd ? '.' : '/public',
       },
     ],
   },

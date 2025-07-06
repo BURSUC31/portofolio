9fdc65ad991d9b2c629cb6399cac3f34d45031b7
Dimitrie Tomulesei <tomuleseidimitrie@gmail.com>
2025-07-06 07:42:41 +0300

    Deploy: Updated portfolio - 2025-07-06 07:42:41

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -41,21 +41,57 @@ const photosCloud = [
 ];
 
 const photosLocal1 = [
-  { src: '/photo1.jpg', alt: 'photo1' },
-  { src: '/photo2.jpg', alt: 'photo2' },
-  { src: '/photo3.jpg', alt: 'photo3' },
-  { src: '/photo4.jpg', alt: 'photo4' },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo1.jpg',
+    alt: 'photo1',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo2.jpg',
+    alt: 'photo2',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo3.jpg',
+    alt: 'photo3',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo4.jpg',
+    alt: 'photo4',
+  },
 ];
 
 const photosLocal2 = [
-  { src: '/photo1.jpg', alt: 'photo1' },
-  { src: '/photo2.jpg', alt: 'photo2' },
-  { src: '/photo3.jpg', alt: 'photo3' },
-  { src: '/photo4.jpg', alt: 'photo4' },
-  { src: '/photo5.jpg', alt: 'photo5' },
-  { src: '/photo6.jpg', alt: 'photo6' },
-  { src: '/photo7.jpg', alt: 'photo7' },
-  { src: '/photo8.jpg', alt: 'photo8' },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo1.jpg',
+    alt: 'photo1',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo2.jpg',
+    alt: 'photo2',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo3.jpg',
+    alt: 'photo3',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo4.jpg',
+    alt: 'photo4',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo5.jpg',
+    alt: 'photo5',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo6.jpg',
+    alt: 'photo6',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo7.jpg',
+    alt: 'photo7',
+  },
+  {
+    src: 'https://storage.googleapis.com/dimitrie-portfolio-website-eu/photo8.jpg',
+    alt: 'photo8',
+  },
 ];
 
 export default function PhotosPage() {
